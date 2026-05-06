f87200d8fd9cc02d929ea42e340412a49815f904
sayo-dev <akinsam2443@gmail.com>
2026-05-06 22:23:46 +0100

    feat: add swagger configuration and update security settings for API documentation

--- src/main/java/org/example/e_market/config/SecurityConfig.java
+++ src/main/java/org/example/e_market/config/SecurityConfig.java
@@ -35,6 +35,19 @@ public class SecurityConfig {
 
     private final String[] WHITE_LIST = {
             "/api/v1/auth/**",
+
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html",
+
+            "/error"
     };
 
     @Bean
---
+++ src/main/java/org/example/e_market/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package org.example.e_market.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "E-MARKET API",
+                description = "Multi-Tenant E-Commerce Marketplace & Inventory System",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        url = "http://localhost:8080",
+                        description = "LOCAL ENV"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "Authentication",
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER
+)
+public class SwaggerConfig {
+}
